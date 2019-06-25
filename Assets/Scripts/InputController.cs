f97804d0ce97a444b763b1715b1aeffc6bc99c31
bli0428 <brandon_li@brown.edu>
2019-06-25 13:27:40 -0400

    added basic meteors/fireballs as well as a fun surprise

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class InputController : MonoBehaviour
+{
+    public SteamVR_Input_Sources InputSource;
+    public SteamVR_Action_Pose ControllerPose;
+
+    public SteamVR_Action_Boolean TriggerPress;
+    public SteamVR_Action_Boolean PadPress;
+    public SteamVR_Action_Boolean GripPress;
+
+    public GameObject MeteorPrefab;
+
+    private float gripTimer;
+
+    private GameObject activeMeteor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gripTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TriggerPress.GetStateDown(InputSource))
+        {
+            activeMeteor = Instantiate(MeteorPrefab, transform.position, transform.rotation);
+        }
+        if(TriggerPress.GetStateUp(InputSource))
+        {
+            activeMeteor.GetComponent<Meteor>().SetActive();
+            Debug.Log(ControllerPose.GetVelocity(InputSource));
+            activeMeteor.GetComponent<Rigidbody>().velocity = 5f * ControllerPose.GetVelocity(InputSource);
+            activeMeteor = null;
+        }
+
+        if(PadPress.GetStateDown(InputSource))
+        {
+            Debug.Log("Pad");
+        }
+
+        if(GripPress.GetStateDown(InputSource))
+        {
+            gripTimer = 0f;
+        }
+        if(GripPress.GetState(InputSource))
+        {
+            gripTimer += Time.deltaTime;
+            if(gripTimer > 2f)
+            {
+                SceneManager.LoadScene("PowerTest");
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(TriggerPress.GetState(InputSource))
+        {
+            activeMeteor.transform.position = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.useGravity = false;
+        Invoke("Despawn", 10f);
+    }
+
+    public void SetActive()
+    {
+        rigid.useGravity = true;
+    }
+
+    private void Despawn()
+    {
+        Destroy(this.gameObject);
+    }
+}
